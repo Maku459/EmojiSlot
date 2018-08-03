332dfd99409f8ea67215850e84184e06370367a0
Kyoko Sakuma <kyokosakuma@kyokonomacbook-ea.local>
2018-08-03 23:40:03 +0900

    Initial Commit

---
+++ random/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  random
+//
+//  Created by Kyoko Sakuma on 2018/08/03.
+//  Copyright © 2018年 Kyoko Sakuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
